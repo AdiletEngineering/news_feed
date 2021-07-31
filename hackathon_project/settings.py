7f1d056baee418b3253a6edd21fdb4052706f091
AdiletEngineering <adilet.joldoshbek@gmail.com>
2021-07-31 15:12:05 +0600

    switched to postgres

--- hackathon_project/settings.py
+++ hackathon_project/settings.py
@@ -77,12 +77,24 @@ WSGI_APPLICATION = 'hackathon_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'discount_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'PORT': '5432'
     }
 }
+
 
 
 # Password validation
